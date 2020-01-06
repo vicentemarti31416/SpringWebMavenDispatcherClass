0d2e241b5831462ec30d0db263172a739fa6eb4c
vicente <vicente@vicente-TUF>
2020-01-06 21:54:55 +0100

    Initial commit

---
+++ web3/src/main/java/com/example/web3/WebConfig.java
@@ -0,0 +1,29 @@
+package com.example.web3;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.example.web3")
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+	@Bean
+	InternalResourceViewResolver getViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		resolver.setRequestContextAttribute("requestContext");
+		return resolver;
+	}
+}
